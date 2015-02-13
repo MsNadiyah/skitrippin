fe63b0e59f4d4be727de4ddb19230ba6146777ac
Nadiyah Ford <nadiyah.ford@gmail.com>
2015-02-13 11:16:28 -0800

    create user sessions

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,11 @@
-angular
-  .module("MainApp", ['ngResource'])
+// Creating the main application in an immediately invoked function expression (IIFE)
+
+// (function(){ 
+  angular
+    .module("MainApp", [
+      'templates',
+      'ngResource',
+      'ngRoute'
+      ]);
+
+// })();  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "static_pages#index"
+  root "static_pages#home"
 
   resources :users, only: [:new, :edit]
 
